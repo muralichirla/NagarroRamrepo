954a3358abb0dce80e3814c7f48dfee96bb0a885
muralichirla <chirlaramareddy8@gmail.com>
2023-07-13 15:32:33 +0530

    Initial commit

---
+++ src/main/java/com/Nagarro/ExitTestBackend/Controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.Nagarro.ExitTestBackend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Nagarro.ExitTestBackend.Dto.EmployeeDto;
+import com.Nagarro.ExitTestBackend.Service.EmployeeService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/employee")
+
+
+
+
+
+public class LoginController {
+	
+	private EmployeeService employeeService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "This java file is working";
+	}
+	@PostMapping(path="/Save")
+	public String saveEmployee(@RequestBody EmployeeDto employeeDto)
+	{
+		String id=employeeService.addEmployee(employeeDto);
+		return id;
+	}
+
+}
